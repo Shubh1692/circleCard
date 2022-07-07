59fc46834fbfba95bd8fae15fffd57da74e0e878
Shubham Vijay Vargiy <Shubh1692@users.noreply.github.com>
2022-07-07 23:24:42 +0530

    Implement Circle Crad in powerbi visual

---
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var circleCard5A243AA87C484CB18837E30867A858EC_DEBUG: IVisualPlugin;
+export default circleCard5A243AA87C484CB18837E30867A858EC_DEBUG;
